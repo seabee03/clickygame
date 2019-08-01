234fe80e7a3654ba59691567a266a2c114eec3f1
seabee03 <darcus.prado@gmail.com>
2019-08-01 15:43:04 -0500

    added code to Section/index.jss

---
+++ src/components/Section/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "../styles/Section.css";
+
+const defaultText = `
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
+architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
+dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
+exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
+consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil
+molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+`;
+// By importing the Section.css file, it is added to the DOM whenever this component loads
+function Section(props) {
+  console.log("SECTION");
+  console.log(props);
+  const colors = props.colors || {
+    backgroundColor: "orange",
+    textColor: "black"
+  }
+  return (
+    <section className="section" style={
+      { backgroundColor: colors.backgroundColor,
+        color: colors.textColor
+      } }>
+      <h2>{props.title || "Lorem ipsum I guess"}</h2>
+      <p>
+        {props.children || defaultText}
+      </p>
+    </section>
+  );
+}
+
+export default Section;
