2cb20e63d128ce80df490a420a540990d880b353
seabee03 <darcus.prado@gmail.com>
2019-07-31 22:39:46 -0500

    added array for random shuffling of cards to App.js

--- src/App.js
+++ src/App.js
@@ -58,4 +58,13 @@ class App extends React.Component {
       };
     });
   };
+
+  // This makes the cards random
+  shuffleCards = array => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
 }
